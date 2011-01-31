6657c1ec714bc5fc5d8ec91c84e337ebbe4f7009
Kevin Smith <kevin@heroku.com>
2011-01-31 08:23:59 -0500

    More integration tests

--- itests/reddy_itest_keys.erl
+++ itests/reddy_itest_keys.erl
@@ -51,3 +51,55 @@ expireat_future_test() ->
     ?WAIT(2000),
     ?assertMatch(0, reddy_keys:exists(C, ?TEST_KEY1)),
     reddy_conn:close(C).
+
+keys_test() ->
+    {ok, C} = ?CONNECT(),
+    reddy_keys:del(C, [?TEST_KEY1, ?TEST_KEY2]),
+    reddy_lists:lpush(C, ?TEST_KEY1, <<"one">>),
+    reddy_lists:lpush(C, ?TEST_KEY2, <<"two">>),
+    ?assertMatch([?TEST_KEY1, ?TEST_KEY2], reddy_keys:keys(C, <<"reddy_*">>)),
+    ?assertMatch([], reddy_keys:keys(C, <<"nokeys*">>)),
+    reddy_keys:del(C, [?TEST_KEY1, ?TEST_KEY2]),
+    reddy_conn:close(C).
+
+ttl_persist_test() ->
+    {ok, C} = ?CONNECT(),
+    reddy_keys:del(C, [?TEST_KEY1]),
+    reddy_lists:lpush(C, ?TEST_KEY1, <<"one">>),
+    ?assertMatch(1, reddy_keys:expire(C, ?TEST_KEY1, 5)),
+    ?assertMatch(5, reddy_keys:ttl(C, ?TEST_KEY1)),
+    ?assertMatch(1, reddy_keys:persist(C, ?TEST_KEY1)),
+    ?assertMatch(-1, reddy_keys:ttl(C, ?TEST_KEY1)),
+    reddy_keys:del(C, [?TEST_KEY1]),
+    reddy_conn:close(C).
+
+random_key_test() ->
+    {ok, C} = ?CONNECT(),
+    reddy_keys:del(C, [?TEST_KEY1, ?TEST_KEY2]),
+    reddy_lists:lpush(C, ?TEST_KEY1, <<"one">>),
+    reddy_lists:lpush(C, ?TEST_KEY2, <<"two">>),
+    Key = reddy_keys:randomkey(C),
+    ?assert(lists:member(Key, [?TEST_KEY1, ?TEST_KEY2])),
+    reddy_keys:del(C, [?TEST_KEY1, ?TEST_KEY2]),
+    reddy_conn:close(C).
+
+rename_test() ->
+    {ok, C} = ?CONNECT(),
+    reddy_keys:del(C, [?TEST_KEY1, ?TEST_KEY2]),
+    reddy_lists:lpush(C, ?TEST_KEY1, <<"one">>),
+    ?assertMatch(ok, reddy_keys:rename(C, ?TEST_KEY1, ?TEST_KEY2)),
+    ?assertMatch([], reddy_keys:keys(C, ?TEST_KEY1)),
+    ?assertMatch([?TEST_KEY2], reddy_keys:keys(C, ?TEST_KEY2)),
+    reddy_keys:del(C, [?TEST_KEY1, ?TEST_KEY2]),
+    reddy_conn:close(C).
+
+renamenx_test() ->
+    {ok, C} = ?CONNECT(),
+    reddy_keys:del(C, [?TEST_KEY1, ?TEST_KEY2]),
+    ?assertMatch({error, _}, reddy_keys:renamenx(C, ?TEST_KEY1, ?TEST_KEY2)),
+    reddy_lists:lpush(C, ?TEST_KEY1, <<"one">>),
+    ?assertMatch(1, reddy_keys:renamenx(C, ?TEST_KEY1, ?TEST_KEY2)),
+    reddy_lists:lpush(C, ?TEST_KEY1, <<"one">>),
+    ?assertMatch(0, reddy_keys:renamenx(C, ?TEST_KEY1, ?TEST_KEY2)),
+    reddy_keys:del(C, [?TEST_KEY1, ?TEST_KEY2]),
+    reddy_conn:close(C).
